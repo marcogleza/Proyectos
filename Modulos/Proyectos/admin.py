e4bdfbe830fa15d54a54507f36a383f42c984f0f
Marco <marcoglez_a@hotmail.com>
2021-06-10 15:56:47 -0500

    Primera Copia

---
+++ Modulos/Proyectos/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from Modulos.Proyectos.models import *
+
+# Register your models here.
+
+admin.site.register(catobjetivo)
+admin.site.register(catareas)
+admin.site.register(catprogramas)
+admin.site.register(catfuentefinancia)
+admin.site.register(catUmedidas)
+admin.site.register(cattipomedicion)
+admin.site.register(catestatus)
+admin.site.register(catumbral)
+admin.site.register(catusuarios)
+admin.site.register(DBproyectos)
---
+++ Modulos/Proyectos/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def formulariocontacto(request):
+    return render(request, "formulariocontacto.html")
+
+def contactar(request):
+    if request.method== "POST":
+        asunto = request.POST["txtAsunto"]
+        mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtemail"]
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["marcogleza@gmail.com"]
+        send_mail(asunto, mensaje, email_desde, email_para,fail_silently=False)
+        return render(request, "contactoexitoso.html")
+    return render(request, "formulariocontacto.html")
+
